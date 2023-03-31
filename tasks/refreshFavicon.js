445ba7d3e7967e9d9b2c9c76178ac23e2bc653d8
Igor Bodrov <bodrovigor02@gmail.com>
2023-03-31 16:53:21 +0300

    fix: 🐛 Добавил фавикон, исправления

--- tasks/refreshFavicon.js
+++ tasks/refreshFavicon.js
@@ -2,7 +2,7 @@ const { src, dest } = require('gulp');
 
 // Обновление favicon в папке build
 module.exports = function refreshFavicon(browserSync) {
-	return src('src/img/favicon/*.+(png|svg|ico)')
+	return src('src/img/favicon/*')
 		.pipe(dest('build/img/favicon/'))
 		.pipe(browserSync.stream())
 };
